9c74a8fde7d6724b4954fc2bf0872a48017c81ff
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-09 20:23:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import HeaderTwo from "./Components/HeaderTwo";
+import Home from "./Components/Home";
+import Checkout from "./Components/Checkout.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <HeaderTwo />
+
+        <Switch>
+          {/* checkout route */}
+          <Route path='/checkout'>
+            <Checkout />
+          </Route>
+
+          {/* home route/ Default route */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
