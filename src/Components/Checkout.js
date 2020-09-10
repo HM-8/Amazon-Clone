2ec0f6fa47b80b37b6715ea29c08a9500c8f3c7f
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-10 16:25:04 +0300

    Update Checkout.js

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -5,37 +5,38 @@ import BasketProduct from "./BasketProduct";
 import { useStateValue } from "../stateProvider";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+	const [{ basket }] = useStateValue();
 
-  return (
-    <div className="checkout">
-      <div className="checkout__left">
-        <img
-          className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/gift-certificates/consumer/2020/GCLP/EGCU_D_1024x180_v1_EN_20200318._CB1584039996_.jpg"
-          alt="gift-card-ad"
-        />
+	return (
+		<div className="checkout">
+			<div className="checkout__left">
+				<img
+					className="checkout__ad"
+					src="https://images-na.ssl-images-amazon.com/images/G/01/gift-certificates/consumer/2020/GCLP/EGCU_D_1024x180_v1_EN_20200318._CB1584039996_.jpg"
+					alt="gift-card-ad"
+				/>
 
-        <div>
-          <h3 className="checkout__title">Your Shopping Basket</h3>
-          
-          {basket.map(item => (
-            <BasketProduct
-            id={item.id}
-            title={item.title}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-          />
-          ))}
-        </div>
-      </div>
+				<div>
+					<h3 className="checkout__title">Your Shopping Basket</h3>
 
-      <div className="checkout__right">
-        <Subtotal />
-      </div>
-    </div>
-  );
+						{basket.map((item) => (
+							<BasketProduct
+								index={0}
+								id={item.id}
+								title={item.title}
+								image={item.image}
+								price={item.price}
+								rating={item.rating}
+							/>
+						))}
+				</div>
+			</div>
+
+			<div className="checkout__right">
+				<Subtotal />
+			</div>
+		</div>
+	);
 }
 
 export default Checkout;
