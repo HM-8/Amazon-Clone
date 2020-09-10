f60418227f89e7fb7c2a04ee667e013286e2278d
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-10 07:17:01 +0300

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,69 +1,74 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import "../css/Login.css";
 import { auth } from "../js/firebase";
 
 function Login() {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const history = useHistory();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const signIn = (e) => {
-    e.preventDefault();
+	const signIn = (e) => {
+		e.preventDefault();
 
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then((auth) => {
-        history.push("/");
-      })
-      .catch((error) => alert(error.message));
-  };
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then((auth) => {
+				if (auth.user) {
+					history.push("/");
+				}
+			})
+			.catch((error) => alert(error.message));
+	};
 
-  const register = (e) => {
-    e.preventDefault();
+	return (
+		<div className="login">
+			<Link className="link" to="/">
+				<img
+					className="login__logo"
+					src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+				/>
+			</Link>
 
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((auth) => {
-        if (auth) {
-          history.push("/");
-        }
-      })
-      .catch((error) => alert(error.message));
-  };
+			<div className="login__container">
+				<h1>Sign-In</h1>
 
-  return (
-    <div className="login">
-      <Link className='link' to="/">
-        <img
-          className="login__logo"
-          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        />
-      </Link>
+				<form>
+					<h5>E-mail</h5>
+					<input
+						type="text"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 
-      <div className="login__container">
-        <h1>Sign-In</h1>
+					<h5>Password</h5>
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
 
-        <form>
-          <h5>E-mail</h5>
-          <input type="text" />
+					<button
+						type="submit"
+						className="login__signInButton"
+						onClick={signIn}
+					>
+						Sign In
+					</button>
+				</form>
 
-          <h5>Password</h5>
-          <input type="password" />
+				<p>
+					By signing-in you agree to Amazon Clone's Conditions of Use & Sale.
+					Please see our Privacy Notice, our Cookies Notice and our
+					Interest-Based Ads Notice.
+				</p>
 
-          <button className="login__signInButton">Sign In</button>
-        </form>
-
-        <p>
-          By creating an account, you agree to Amazon Clone's Conditions of Use and
-          Privacy Notice.
-        </p>
-
-        <button className="login__registerInButton">
-          Create your Amazone Account
-        </button>
-      </div>
-    </div>
-  );
+				<Link className="link" to="/createAccount">
+					<button>Create your Amazon Account</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
