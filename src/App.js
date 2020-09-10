0003948bd618d03848983bc83029bba688916617
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-10 07:14:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import HeaderTwo from "./Components/HeaderTwo";
 import Home from "./Components/Home";
 import Checkout from "./Components/Checkout.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Footer from "./Components/Footer";
+import CreateAccount from "./Components/CreateAccount";
+import { auth } from "./js/firebase";
+import { useStateValue } from "./stateProvider";
 
 function App() {
-  return (
-    <Router>
-      <div className='app'>
-        <Header />
-        <HeaderTwo />
+	const [{}, dispatch] = useStateValue();
 
-        <Switch>
-          {/* checkout route */}
-          <Route path='/checkout'>
-            <Checkout />
-          </Route>
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				dispatch({
+					type: "SET_USER",
+					user: authUser,
+				});
+			} else {
+				dispatch({
+					type: "SET_USER",
+					user: null,
+				});
+			}
+		});
+	}, []);
 
-          {/* home route/ Default route */}
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					{/* login route */}
+					<Route path="/login">
+						<Login />
+					</Route>
+
+					{/* createaccount route */}
+					<Route path="/createAccount">
+						<CreateAccount />
+					</Route>
+
+					{/* checkout route */}
+					<Route path="/checkout">
+						<Header />
+						<HeaderTwo />
+						<Checkout />
+						<Footer />
+					</Route>
+
+					{/* home route/ Default route */}
+					<Route path="/">
+						<Header />
+						<HeaderTwo />
+						<Home />
+						<Footer />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
