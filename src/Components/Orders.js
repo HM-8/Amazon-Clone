853206e7bfee889b35b789e88ec71035e4479f6a
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-13 00:31:37 +0300

    Add Orders.js

---
+++ src/Components/Orders.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Orders.css";
+
+function Orders() {
+	return (
+		<div className="orders">
+			<div className="orders__container">
+				<h1>Orders</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Orders;
