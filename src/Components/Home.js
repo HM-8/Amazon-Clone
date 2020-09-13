7d288ed5cfef3a23348d66aa945e5be343019084
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-13 10:45:46 +0300

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,58 +8,55 @@ function Home() {
 			<div className="home__container">
 				<img
 					className="home__image"
-					src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_2x._CB432458382_.jpg"
+					src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Currency_v2_en_US_2x._CB428993290_.jpg"
 					alt=""
 				/>
 
 				<div className="home__row">
 					<Product
 						id={1}
-						title="HP VH240a 23.8-inch Full HD 1080p IPS LED Monitor with Built-in Speakers and VESA Mounting, Rotating Portrait & Landscape, Tilt, and HDMI & VGA Ports (1KL30AA) - Black"
-						price={109.99}
-						image="https://images-na.ssl-images-amazon.com/images/I/31PTviHMeUL._AC_US240_FMwebp_QL65_.jpg"
+						title="Dell Inspiron 27 AIO, 27 inch, FHD Touchscreen, Intel Core i7-10510U, NVIDIA MX110 2GB, 1TB HDD + 512GB SSD Storage, 16GB RAM, i7790-7388SLV-PUS, 15-15.99 inches"
+						price={1,333.98}
+						image="https://images-na.ssl-images-amazon.com/images/I/717mmm0oqnL._AC_SL1500_.jpg"
 						rating={4}
 					/>
 
 					<Product
 						id={2}
 						title={
-							"Lenovo Flex 5 14" +
-							"2-in-1 Laptop, 14.0" +
-							"FHD (1920 x 1080) Touch Display, AMD Ryzen 5 4500U Processor, 16GB DDR4, 256GB SSD, AMD Radeon Graphics, Digital Pen Included, Win 10, 81X20005US, Graphite Grey"
+							"Apple MacBook Air (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray (Latest Model)"
 						}
-						price={595.69}
-						image="https://images-na.ssl-images-amazon.com/images/I/41Sj6WWOXtL._AC_US240_FMwebp_QL65_.jpg"
-						rating={4}
+						price={949.99}
+						image="https://images-na.ssl-images-amazon.com/images/I/71k3fJh5EwL._AC_SL1500_.jpg"
+						rating={5}
 					/>
 				</div>
 
 				<div className="home__row">
 					<Product
 						id={3}
-						title="AUKEY FHD Webcam, 1080p Live Streaming Camera with Stereo Microphone, Desktop or Laptop USB Webcam for Widescreen Video Calling and Recording"
-						price={49.99}
-						image="https://images-na.ssl-images-amazon.com/images/I/310SV0HFMPL._AC_US240_FMwebp_QL65_.jpg"
-						rating={4}
+						title="Apple iPad (10.2-inch, Wi-Fi, 32GB) - Space Gray (Latest Model)"
+						price={329.99}
+						image="https://images-na.ssl-images-amazon.com/images/I/41h0H4SCgPL._AC_US160_.jpg"
+						rating={5}
 					/>
 
 					<Product
 						id={4}
-						title="WD 2TB WD Elements Portable External Hard Drive, USB 3.0 - WDBU6Y0020BBK-WESN"
-						price={49.72}
-						image="https://images-na.ssl-images-amazon.com/images/I/31pLxnRHbsL._AC_US240_FMwebp_QL65_.jpg"
+						title="Wyze Cam Pan 1080p Pan/Tilt/Zoom Wi-Fi Indoor Smart Home Camera with Night Vision, 2-Way Audio, Works with Alexa & the Google Assistant, White - WYZECP1"
+						price={50.96}
+						image="https://images-na.ssl-images-amazon.com/images/I/31Y8NH8Ia5L._AC_.jpg"
 						rating={4}
 					/>
 
 					<Product
 						id={5}
 						title={
-							"Sceptre E248W-19203R 24" +
-							"Ultra Thin 75Hz 1080p LED Monitor 2x HDMI VGA Build-in Speakers, Metallic Black 2018"
+							"Beats Solo3 Wireless On-Ear Headphones - Apple W1 Headphone Chip, Class 1 Bluetooth, 40 Hours Of Listening Time - Black (Latest Model)"
 						}
-						price={104.68}
-						image="https://images-na.ssl-images-amazon.com/images/I/51d3J1EYbVL._AC_US240_FMwebp_QL65_.jpg"
-						rating={5}
+						price={179.00}
+						image="https://m.media-amazon.com/images/I/51QxA-98Q+L._AC_UL320_.jpg"
+						rating={4}
 					/>
 				</div>
 
@@ -68,10 +65,10 @@ function Home() {
 					<Product
 						id={6}
 						title={
-							"Sony X800H 65 Inch TV: 4K Ultra HD Smart LED TV with HDR and Alexa Compatibility - 2020 Model"
+							"Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED Computer Monitor, 3840 x 1080p Resolution, 1ms Response, FreeSync 2 with HDR"
 						}
-						price={903.09}
-						image="https://m.media-amazon.com/images/I/71+F+eS7qfL._QL65_AC_UL640_.jpg"
+						price={999.99}
+						image="https://m.media-amazon.com/images/I/81Zt42ioCgL._AC_UY218_.jpg"
 						rating={4}
 					/>
 				</div>
