afd9d10d8484aa41ec90887c3fec9c3758fce341
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-09-13 04:36:34 +0300

    Update axios.js

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: '...' // The API (cloud function) URL
+    baseURL: 'http://localhost:9000/.netlify/functions/app' // The API (cloud function) URL
 });
 
 export default instance;
